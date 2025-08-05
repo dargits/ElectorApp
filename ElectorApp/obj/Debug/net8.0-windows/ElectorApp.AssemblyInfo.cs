2e5310d2752fe5b930d91dea2ca3fa552869ebba
Inc Shy <imshydev@gmail.com>
2025-08-05 19:34:27 +0700

    DSDSDFSD

--- ElectorApp/obj/Debug/net8.0-windows/ElectorApp.AssemblyInfo.cs
+++ ElectorApp/obj/Debug/net8.0-windows/ElectorApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ElectorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595334cc06f10afa68b20af078903cc1dfc7631f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080afd78e86a969df6288e2fad643b320f5f6a10")]
 [assembly: System.Reflection.AssemblyProductAttribute("ElectorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ElectorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
